d7bd2e7a6178c191dc27c5cc59a542c2ae396a4a
Jeremie Patterson <jepate@l4714-06.info.polymtl.ca>
2022-03-08 11:14:32 -0500

    syracuse rec working

--- syracuse/syracuse_s_rec.s
+++ syracuse/syracuse_s_rec.s
@@ -8,9 +8,16 @@ pushl %ebx
 # DEBUT COMPLETION
 
 init:
-movl 16(%esp), %ecx
 movl 12(%esp), %eax
-jmp print
+movl 16(%esp), %ecx
+
+print:
+pushl %ecx
+pushl %eax
+call afficher
+popl %eax
+popl %ecx
+incl %ecx
 
 check_number:
 cmpl $1, %eax
@@ -26,22 +33,15 @@ odd:
 movl $3, %ebx
 mul %ebx
 addl $1, %eax
-jmp print
+jmp call_again
 
 even:
 movl $0, %edx
 movl $2, %ebx
 idiv %ebx
-jmp print
 
-print:
-pushl %ecx
-pushl %eax
-call afficher
-popl %eax
-popl %ecx
-incl %ecx
-
+call_again:
+popl %ebx
 pushl %ecx
 pushl %eax
 call syracuse_s_rec
